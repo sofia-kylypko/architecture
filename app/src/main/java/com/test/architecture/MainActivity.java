6bc8561514422388d1ffc5462e071a52ff86ec78
sofia <sofia.kilipko@gmail.com>
2020-12-07 19:20:17 +0200

    add new activity

--- app/src/main/java/com/test/architecture/MainActivity.java
+++ app/src/main/java/com/test/architecture/MainActivity.java
@@ -2,13 +2,23 @@ package com.test.architecture;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnStart=findViewById(R.id.btnStart);
+
+        btnStart.setOnApplyWindowInsetsListener(view->{
+            Intent intent= new Intent(this,SecondActivity.class);
+            startActivity(intent);
+        });
     }
 }
