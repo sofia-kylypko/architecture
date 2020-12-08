8ea47d08d2e073b82ebced65abeb3f20ad72e90d
sofia <sofia.kilipko@gmail.com>
2020-12-08 19:59:54 +0200

    add new activity

--- app/src/main/java/com/test/architecture/SecondActivity.java
+++ app/src/main/java/com/test/architecture/SecondActivity.java
@@ -2,6 +2,7 @@ package com.test.architecture;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SecondActivity extends AppCompatActivity {
@@ -10,5 +11,12 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        btnStart2=findViewById(R.id.btnStart2);
+
+        btnStart2.setOnClickListener(view->{
+            Intent intent= new Intent(this,SecondActivity.class);
+            startActivity(intent);
+        });
     }
 }
