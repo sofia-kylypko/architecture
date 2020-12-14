9187217dce138ff141d6e0f68da23b6145e9d6be
sofia <sofia.kilipko@gmail.com>
2020-12-14 16:59:46 +0200

    add new commit

--- app/src/main/java/com/test/architecture/SplashActivity.java
+++ app/src/main/java/com/test/architecture/SplashActivity.java
@@ -1,12 +1,10 @@
 package com.test.architecture;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Message;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
     public static final int DILAY_IN_MILLIS=2000;
