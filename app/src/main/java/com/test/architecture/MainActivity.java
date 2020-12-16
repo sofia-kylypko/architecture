7248684dcefc231e8de66e9eaf3f27f971a55d45
sofia <sofia.kilipko@gmail.com>
2020-12-16 17:30:33 +0200

    add checkers for password

--- app/src/main/java/com/test/architecture/MainActivity.java
+++ app/src/main/java/com/test/architecture/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import static com.test.architecture.Utils.password;
+
 public class MainActivity extends AppCompatActivity {
     Button btnLogin;
     EditText etLogin;
@@ -19,31 +21,85 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnLogin=findViewById(R.id.btnLogin);
-        etLogin=findViewById(R.id.etLogin);
-        etPassword=findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        etLogin = findViewById(R.id.etLogin);
+        etPassword = findViewById(R.id.etPassword);
 
-        btnLogin.setOnClickListener(view->{
-            if(checkLogin(etLogin.getText().toString())&& checkPassword(etPassword.getText().toString())){
-                startActivity();
-            } else{
-                Toast.makeText(this,"Lodin or password is incorect",Toast.LENGTH_SHORT).show();
+        btnLogin.setOnClickListener(view -> {
+            if (checkLogin(etLogin.getText().toString()) && checkPasswordIncludeNumbers(etPassword.getText().toString()) && checkPasswordCapitalsLetters(etPassword.getText().toString()) && checkPasswordLength(etPassword.getText().toString())) {
+                if (isPasswordValid(etPassword.getText().toString())) {
+                    startActivity();
+                }else{
+                    Toast.makeText(this, "Login or password is incorect", Toast.LENGTH_SHORT).show();
+                }
             }
-
         });
     }
 
-    private void startActivity(){
-        Intent intent= new Intent(this,SecondActivity.class);
-        finish();
-    }
-    private boolean checkLogin(String login){
-        for(int i=0; i<Utils.logins.length; i++){
-            if(Utils.logins[i].equals(login))return true;
+    private boolean checkLogin(String login) {
+        for (int i = 0; i < Utils.logins.length; i++) {
+            if (Utils.logins[i].equals(login)) return true;
         }
         return false;
     }
-    private boolean checkPassword(String password){
-        return password.equals(Utils.password);
+
+
+    private void startActivity() {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+
+    private boolean checkPasswordIncludeNumbers(String password) {
+        char[] array = password.toCharArray();
+        for (char x : array) {
+            if (Character.isDigit(x)) {
+                return true;
+            }
+        }
+
+        Toast.makeText(this, "Password need to include numbers ", Toast.LENGTH_SHORT).show();
+        return false;
+
+    }
+
+
+    private boolean checkPasswordCapitalsLetters(String password) {
+        boolean resultA = false;
+        boolean resultB = false;
+        char[] array = password.toCharArray();
+        for (char x : array) {
+            if (Character.isUpperCase(x)) {
+                resultA = true;
+            } else {
+                resultB = true;
+            }
+        }
+        if (resultA && resultB) {
+            return true;
+        } else {
+            Toast.makeText(this, "Password need to include upper and lower letters ", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+
+    private boolean checkPasswordLength(String password) {
+        if (password.length() > 7) {
+            return true;
+        } else {
+            Toast.makeText(this, "Password need to include eight or more symbols", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+    private boolean isPasswordValid(String password) {
+        String tmpPassword=Utils.password;
+        if (tmpPassword.equals(password)){
+            return true;
+        }else {
+            return false;
+        }
     }
 }
--- app/src/main/java/com/test/architecture/Utils.java
+++ app/src/main/java/com/test/architecture/Utils.java
@@ -2,5 +2,6 @@ package com.test.architecture;
 
 public class Utils {
     public static String[] logins={"login1","login2"};
-    public static String password="pass";
+    public static String password="Password1";
+
 }
